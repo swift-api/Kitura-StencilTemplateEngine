cdbbb36e3763f8225bb320cbb494ea5aa49f5bf9
Vadim Eisenberg <vadime@il.ibm.com>
2016-05-16 10:27:22 +0300

    incremented the version of Kitura-TemplateEngine dependency

--- Package.swift
+++ Package.swift
@@ -19,7 +19,6 @@ import PackageDescription
 let package = Package(
     name: "KituraStencil",
     dependencies: [.Package(url: "https://github.com/IBM-Swift/Kitura-TemplateEngine.git",
-                            versions: Version(0,0,0)..<Version(0,1,0)),
-                     .Package(url: "https://github.com/kylef/Stencil",
-                              majorVersion: 0)])
+                            majorVersion: 0, minor: 13),
+                   .Package(url: "https://github.com/kylef/Stencil", majorVersion: 0)])
 
